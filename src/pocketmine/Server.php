bestlucky0825/PocketMine-MP

commit a6e5b6e158ff82da1a5ee1f12376c37d4f25114c

Log a debug message when not sending crash due to folder plugin

src/pocketmine/Server.php
@@ -2280,7 +2280,7 @@ class Server{
 				if(is_string($plugin)){
 					$p = $this->pluginManager->getPlugin($plugin);
 					if($p instanceof Plugin and !($p->getPluginLoader() instanceof PharPluginLoader)){
-						$report = false;
+						$this->logger->debug("Not sending crashdump due to caused by non-phar plugin");
 					}
 				}
 

commit 193a4a7ed64c71e307cd30543f0462e9d8558be9

Server: fixed crash reporting bug introduced by a6e5b6e158ff82da1a5ee1f12376c37d4f25114c

src/pocketmine/Server.php
@@ -2271,6 +2271,7 @@ class Server{
 					$p = $this->pluginManager->getPlugin($plugin);
 					if($p instanceof Plugin and !($p->getPluginLoader() instanceof PharPluginLoader)){
 						$this->logger->debug("Not sending crashdump due to caused by non-phar plugin");
+						$report = false;
 					}
 				}
 